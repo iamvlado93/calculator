93bcbad209976f3e43072167f881e7d3f177594c
iamvlado93 <vladrjeousski@gmail.com>
2021-01-05 13:18:32 +0300

    feat: add content

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,11 +1,29 @@
-const inputData = document.querySelector('.formula'),
-      resultData = document.querySelectorAll('.result');
+const display = document.querySelector('.formula');
+const calculation = document.querySelector('.result');
+
 
 function input(i) {
-    inputData.value = inputData.value + i
-}
+    display.value = display.value + i
+  }
 
 function result() {
-    resultData.value = eval(inputData.value);
-    inputData.value = eval(inputData.value);
+    if (eval(display.value) == undefined) {
+        calculation.value = '0';
+        display.value = '0';
+    } if (eval(display.value) == Infinity) {
+        calculation.value = '0';
+        display.value = 'Not Allowed';
+    }
+    calculation.value = eval(display.value);
+    display.value = eval(display.value);
 }
+
+function reset() {
+    calculation.value = '';
+    display.value = '';
+}
+
+function percentage() {
+    calculation.value = calculation.value / 100;
+}
+
