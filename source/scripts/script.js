c20707e8e56ba0118b26998e6257d417b4018fd2
iamvlado93 <vladrjeousski@gmail.com>
2021-03-30 20:51:58 +0300

    feat: restructured calculator

--- source/scripts/script.js
+++ source/scripts/script.js
@@ -1,29 +1,31 @@
-const display = document.querySelector('.formula');
-const calculation = document.querySelector('.result');
+const output2 = document.querySelector('.output2');
+const output1 = document.querySelector('.output1');
+const btns = document.querySelectorAll('.btn');
+const equal = document.querySelector('.equal');
+const clear = document.querySelector('.clear');
+const clearAll = document.querySelector('.clearAll');
 
+btns.forEach(btn => {
+    btn.addEventListener('click', () => {
+        let content = btn.getAttribute('data-num');
+        output2.textContent += content;
+    })
+})
 
-function input(i) {
-    display.value = display.value + i
-  }
+clearAll.addEventListener('click', () => {
+    output2.textContent = '';
+    output1.textContent = '';
+})
 
-function result() {
-    if (eval(display.value) == undefined) {
-        calculation.value = '0';
-        display.value = '0';
-    } if (eval(display.value) == Infinity) {
-        calculation.value = '0';
-        display.value = 'Not Allowed';
-    }
-    calculation.value = eval(display.value);
-    display.value = eval(display.value);
-}
+equal.addEventListener('click', () => {
+    let expression = output2.textContent;
+    output2.textContent = eval(expression);
+    output1.textContent = eval(expression);
+})
 
-function reset() {
-    calculation.value = '';
-    display.value = '';
-}
-
-function percentage() {
-    calculation.value = calculation.value / 100;
-}
-
+clear.addEventListener('click', () => {
+    let str = output2.textContent;
+    let arr = str.split('');
+    arr.pop();
+    output2.textContent = arr.join('');
+})
